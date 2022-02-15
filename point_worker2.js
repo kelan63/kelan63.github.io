3815637430da9ae5cc822907a85c926c4f5820e8
kjm <kjouquan@enssat.fr>
2022-02-15 10:44:10 +0100

    rectification1

--- point_worker2.js
+++ point_worker2.js
@@ -2,7 +2,7 @@ onmessage = function coordonnees(e) {
     var x =  Math.floor(Math.random()*1100);
     var y =  Math.floor(Math.random()*1100);
     postMessage([x,y,e[0]]);
-    setTimeout("coordonnees()",1000);
+    setTimeout("coordonnees(e)",1000);
   }
 
   
